70d517764024ab861bfb65e50ab13621cfcf6e51
humyna <hufo125@gmail.com>
2021-11-21 12:11:54 +0800

    验证body请求参数为kv pair
    编译命令
    cargo build --quiet && target/debug/httpie post httpbin.org/post a=1 b
    cargo build --quiet && target/debug/httpie post https://httpbin.org/post a=1 b

--- 3.learningbycoding/class04/httpie/src/main.rs
+++ 3.learningbycoding/class04/httpie/src/main.rs
@@ -1,6 +1,8 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use reqwest::Url;
 use clap::{AppSettings, Clap};
+use std::str::FromStr;
+
 
 //定义httpie的CLI主入口，它包含若干子命令
 // 下面/// 注释是文档, clap会将其作为CLI的帮助
@@ -33,9 +35,45 @@ fn parse_url(s: &str) -> Result<String> {
 
 #[derive(Clap, Debug)]
 struct Post {
+    /// HTTP 请求的 URL
+    #[clap(parse(try_from_str =  parse_url))]
     url : String, 
-    body : Vec<String>,
+    /// HTTP 请求的 body
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body : Vec<KvPair>,
 }
+
+/// 命令行中的key=value可以通过parse_kv_pair解析成KvPair
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+/// 当我们实现 FromStr trait后，可以用str.parse()将方法字符串解析成KvPair
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+    
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 使用=进行split，会得到一个迭代器
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse{}", s));
+        Ok(Self {
+            // 从迭代器中取第一个结果作为key，迭代器返回Some(T)/None
+            // 我们将其转换成 Ok(T)/Err(E), 然后用E 处理错误
+            k: (split.next().ok_or_else(err)?).to_string(),
+            // 从迭代器中取出第二个结果作为value
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+/// 因为我们为KvPair实现了FromStr，所以我们可以直接使用s.parse()得到KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+
 fn main() {
     let opts: Opts= Opts::parse();
     println!("{:?}", opts);
