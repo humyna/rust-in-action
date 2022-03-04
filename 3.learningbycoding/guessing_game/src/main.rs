7573d33c3b71849aae251bf75d485632d9fac807
humyna <hufo125@gmail.com>
2022-03-04 21:24:32 +0800

    fix: handle exception

--- 3.learningbycoding/guessing_game/src/main.rs
+++ 3.learningbycoding/guessing_game/src/main.rs
@@ -5,8 +5,8 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("The secret number is: {}", secret_number);
-    
+    //println!("The secret number is: {}", secret_number);
+
     loop{
         println!("Please input your guess.");
 
@@ -16,7 +16,13 @@ fn main() {
             .read_line(&mut guess) //The full job of read_line is to take whatever the user types into standard input and append that into a string (without overwriting its contents), so we therefore pass that string as an argument. The string argument needs to be mutable so the method can change the string’s content.
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("you need to input a number!");
+                continue;
+            }
+        };
 
         println!("You guessed: {}", guess);
 
@@ -24,7 +30,7 @@ fn main() {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!"),
+                println!("You win!");
                 break;
             }
         }
