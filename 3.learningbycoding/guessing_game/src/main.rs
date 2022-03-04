cb2e3ceea78b4685241679c8e7cb9cc49bd786aa
humyna <hufo125@gmail.com>
2022-03-04 20:19:54 +0800

    feat: guessing game: generate a random number

--- 3.learningbycoding/guessing_game/src/main.rs
+++ 3.learningbycoding/guessing_game/src/main.rs
@@ -1,9 +1,10 @@
 use std::io;
-//https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
-// to be continued
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
 
     let mut guess = String::new();
