88572a8fb004fa32741deddc462eb1f5e88e0793
humyna <hufo125@gmail.com>
2022-03-04 21:15:39 +0800

    feat: guessing game: allow multiple guesses with looping

--- 3.learningbycoding/guessing_game/src/main.rs
+++ 3.learningbycoding/guessing_game/src/main.rs
@@ -1,17 +1,32 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
+    
+    loop{
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess) //The full job of read_line is to take whatever the user types into standard input and append that into a string (without overwriting its contents), so we therefore pass that string as an argument. The string argument needs to be mutable so the method can change the string’s content.
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess) //The full job of read_line is to take whatever the user types into standard input and append that into a string (without overwriting its contents), so we therefore pass that string as an argument. The string argument needs to be mutable so the method can change the string’s content.
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp( &secret_number ) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!"),
+                break;
+            }
+        }
+    }
 }
